ac4b5a9608a9f8f4653fcff93faf4f509e7ebf4b
xl1921 <xuefan.li22@imperial.ac.uk1>
2022-11-22 11:16:43 +0000

    add

--- armageddon/locator.py
+++ armageddon/locator.py
@@ -34,9 +34,49 @@ def great_circle_distance(latlon1, latlon2):
         print(great_circle_distance([[54.0, 0.0], [55, 0.0]], [55, 1.0]))
     [1.286e+05 6.378e+04]
     """
+    Rp = 6371000
+    
+    
+    latlon1 = np.array(latlon1)
+    latlon2 = np.array(latlon2)
+    
+    if latlon1.ndim == 1:
+        latlon1 = latlon1.reshape(1,2)
 
+    if latlon2.ndim == 1:
+        latlon2 = latlon2.reshape(1,2)
+    
     distance = np.empty((len(latlon1), len(latlon2)), float)
+    lat1 = latlon1[:, 0]
+    lat2 = latlon2[:, 0]
+    lon1 = latlon1[:, 1]
+    lon2 = latlon2[:, 1]
+    #print(lat1)
+
+    for i in range(len(latlon1)):
+        
+        for j in range(len(latlon2)):
+            #print(lat1[i])
+            # print(lat2[j])
+            # print(lon1[i])
+            # print(lon2[j])
+            #dis = 2*Rp*np.arcsin(np.sqrt((np.sin(abs(lat1[i]-lat2[j])/2))**2+np.cos(lat1[i])*np.cos(lat2[j])*(np.sin(abs(lon1[i]-lon2[j])/2))**2))
+            #dis = Rp*np.arccos(np.sin(lat1[i])*np.sin(lat2[j])+np.cos(lat1[i])*np.cos(lat2[j])*np.cos(abs(lon1[i]-lon2[j])))
+            num = np.sqrt((np.cos(lat2[j])*np.sin(abs(lon1[i]-lon2[j])))**2+(np.cos(lat1[i])*np.sin(lat2[j])-np.sin(lat1[i])*np.cos(lat2[j])*np.cos(abs(lon1[i]-lon2[j])))**2)
+            den = np.sin(lat1[i])*np.sin(lat2[j])+np.cos(lat1[i])*np.cos(lat2[j])*np.cos(abs(lon1[i]-lon2[j]))
+            dis = Rp*np.arctan(num/den)
+            print(num, den)
+            
+            distance[i][j] = dis
     return distance
+
+#fmt = lambda x: np.format_float_scientific(x, precision=3)
+#with np.printoptions(formatter={'all', fmt}):
+    #print(great_circle_distance([[54.0, 0.0], [55, 0.0]], [55, 1.0]))
+pnts1 = np.array([[54.0, 0.0], [55.0, 1.0], [54.2, -3.0]])
+pnts2 = np.array([[55.0, 1.0], [56.0, -2.1], [54.001, -0.003]])
+print(great_circle_distance(pnts1, pnts2))
+#print(np.array([55, 1.0]).reshape(1,2))
 
 
 class PostcodeLocator(object):
